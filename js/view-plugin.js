c560e244e6e28daeed3b08f73d71f1c6c4359323
gusssar <dassler47@gmail.com>
2019-02-20 15:16:36 +0300

    rebuild about-chart

--- js/view-plugin.js
+++ js/view-plugin.js
@@ -1,3 +1,4 @@
+/**добавление классов для изменения видимости, анимации и появления страниц без javascript'ов*/
 $(document).ready(function() {
     $('.content-view').viewportChecker({
         classToAdd: 'visible animated zoomIn',
@@ -9,7 +10,7 @@ $(document).ready(function() {
         //scrollHorizontal: false
 });
 });
-
+/**добавление классов для изменения видимости, анимации и появления страниц с javascript'ом*/
 $(document).ready(function() {
     $('.content-view-2').viewportChecker({
         classToAdd: 'visible animated zoomIn',
@@ -23,7 +24,7 @@ $(document).ready(function() {
         //scrollHorizontal: false
     });
 });
-
+/**добавление классов для изменения видимости, анимации и появления страниц с javascript'ом*/
 $(document).ready(function() {
     $('.content-view-3').viewportChecker({
         classToAdd: 'visible animated zoomIn',
@@ -37,49 +38,50 @@ $(document).ready(function() {
         //scrollHorizontal: false
     });
 });
-
+/**добавление классов для начала анимации при прокрутки до страницы*/
 $(document).ready(function() {
-    $('.fb').viewportChecker({
-        classToAdd: 'fb-anim',
-        offset: 100,
-        repeat: false
-    });
+    $('.fb').viewportChecker({classToAdd: 'fb-anim'});
+    $('.gh').viewportChecker({classToAdd: 'gh-anim'});
+    $('.st').viewportChecker({classToAdd: 'st-anim'});
+    $('.tw').viewportChecker({classToAdd: 'tw-anim'});
+    $('.vk').viewportChecker({classToAdd: 'vk-anim'});
+    $('.mail').viewportChecker({classToAdd: 'mail-anim'});
 });
-
-$(document).ready(function() {
-    $('.gh').viewportChecker({
-        classToAdd: 'gh-anim',
-        offset: 100,
-        repeat: false
-    });
-});
-$(document).ready(function() {
-    $('.st').viewportChecker({
-        classToAdd: 'st-anim',
-        offset: 100,
-        repeat: false
-    });
-});
-$(document).ready(function() {
-    $('.tw').viewportChecker({
-        classToAdd: 'tw-anim',
-        offset: 100,
-        repeat: false
-    });
-});
-
-$(document).ready(function() {
-    $('.vk').viewportChecker({
-        classToAdd: 'vk-anim',
-        offset: 100,
-        repeat: false
-    });
-});
-
-$(document).ready(function() {
-    $('.mail').viewportChecker({
-        classToAdd: 'mail-anim',
-        offset: 100,
-        repeat: false
-    });
-});
+//
+// $(document).ready(function() {
+//     $('.gh').viewportChecker({
+//         classToAdd: 'gh-anim',
+//         offset: 100,
+//         repeat: false
+//     });
+// });
+// $(document).ready(function() {
+//     $('.st').viewportChecker({
+//         classToAdd: 'st-anim',
+//         offset: 100,
+//         repeat: false
+//     });
+// });
+// $(document).ready(function() {
+//     $('.tw').viewportChecker({
+//         classToAdd: 'tw-anim',
+//         offset: 100,
+//         repeat: false
+//     });
+// });
+//
+// $(document).ready(function() {
+//     $('.vk').viewportChecker({
+//         classToAdd: 'vk-anim',
+//         offset: 100,
+//         repeat: false
+//     });
+// });
+//
+// $(document).ready(function() {
+//     $('.mail').viewportChecker({
+//         classToAdd: 'mail-anim',
+//         offset: 100,
+//         repeat: false
+//     });
+// });

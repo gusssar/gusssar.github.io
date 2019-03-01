13760b0712b86ecceb317b2edbfc4254380963ba
gusssar <dassler47@gmail.com>
2019-03-01 11:52:52 +0300

    add fake-glass in mobile

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,25 @@
 let show_menu = document.getElementById('showMenu');
 let close_menu = document.getElementById('closeMenu');
 let header = document.getElementsByTagName("header");
+let fake_glass = document.getElementById('fake-glass');
 let nav_hello    = document.getElementById('id-nav-hello-1');
 let nav_intro = document.getElementById('id-nav-intro-1');
 let nav_skills   = document.getElementById('id-nav-skills-1');
 let nav_works   = document.getElementById('id-nav-works-1');
 let nav_about = document.getElementById('id-nav-about-1');
 let nav_contact = document.getElementById('id-nav-contact-1');
-let _content_body = document.getElementById('content-body');
+// let _content_body = document.getElementById('content-body');
 
 let hide_menu = () => {
     header[0].style.display = 'none';
     show_menu.style.display = "block";
+    fake_glass.style.display = 'none'
 };
 
 show_menu.onclick =  () => {
     header[0].style.display = 'block';
     show_menu.style.display = "none";
+    fake_glass.style.display = 'block'
 };
 
 
@@ -27,4 +30,4 @@ nav_skills.onclick = () => {hide_menu()};
 nav_works.onclick = () => {hide_menu()};
 nav_about.onclick = () => {hide_menu()};
 nav_contact.onclick = () => {hide_menu()};
-_content_body.onclick = () => {hide_menu()};
+fake_glass.onclick = () => {hide_menu()};

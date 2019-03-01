650f84553a8c68c1c260ff3fb73031e1ba8aa45b
gusssar <dassler47@gmail.com>
2019-03-01 15:26:11 +0300

    fix tag[issue#2]

--- js/menu.js
+++ js/menu.js
@@ -22,6 +22,18 @@ show_menu.onclick =  () => {
     fake_glass.style.display = 'block'
 };
 
+let a;
+if (navigator.userAgent.search(/Safari/) > 0) {a = 'Safari'};
+if (navigator.userAgent.search(/Firefox/) > 0) {a = 'MozillaFirefox'};
+if (navigator.userAgent.search(/MSIE/) > 0 || navigator.userAgent.search(/NET CLR /) > 0) {a = 'Internet Explorer'};
+if (navigator.userAgent.search(/Chrome/) > 0) {a = 'Google Chrome'};
+if (navigator.userAgent.search(/YaBrowser/) > 0) {a = 'Яндекс браузер'};
+if (navigator.userAgent.search(/OPR/) > 0) {a = 'Opera'};
+if (navigator.userAgent.search(/Konqueror/) > 0) {a = 'Konqueror'};
+if (navigator.userAgent.search(/Iceweasel/) > 0) {a = 'Debian Iceweasel'};
+if (navigator.userAgent.search(/SeaMonkey/) > 0) {a = 'SeaMonkey'};
+if (navigator.userAgent.search(/Edge/) > 0) {a = 'Microsoft Edge'};
+console.log(a);
 
 close_menu.onclick = () => {hide_menu()};
 nav_hello.onclick = () => {hide_menu()};

da8622a3c9c82b42af394f0eb3102e7bc6eec543
gusssar <dassler47@gmail.com>
2019-08-07 16:45:38 +0300

    добавил попап

--- slides/index.js
+++ slides/index.js
@@ -2,74 +2,85 @@ const data={
     "pic_0": {
         "href":'img/jpg/w2560/bulletin-board.jpg',
         "alt":"Доска объявлений",
-        "data-path":"https://github.com/gusssar/bulletin-board",
-        "title":"\"Доска объявлениий\" реализованное с помощью React.js"
+        "data_path":"https://github.com/gusssar/bulletin-board",
+        "title":"\"Доска объявлениий\" реализованная с помощью React.js"
     },
     "pic_1": {
         "href":"img/jpg/w2560/djino.jpg",
         "alt":"Промо-страница",
-        "data-path":"https://github.com/gusssar/DjiniTestSecond",
+        "data_path":"https://github.com/gusssar/DjiniTestSecond",
         "title":"Промо-странца"
     },
     "pic_2": {
         "href":"img/jpg/w2560/table.jpg",
         "alt":"Табло Шереметьево",
-        "data-path":"https://github.com/gusssar/table-air",
+        "data_path":"https://github.com/gusssar/table-air",
         "title":"Табло Шереметьево реализованное с помощью React.js"
     },
     "pic_3": {
         "href":"img/jpg/w2560/yandex_home.jpg",
         "alt":"Яндекс.Дом",
-        "data-path":"https://github.com/gusssar/entrance_task_2",
+        "data_path":"https://github.com/gusssar/entrance_task_2",
         "title":"Макет страницы &quotЯндекс.Дом&quot"
     },
     "pic_4": {
         "href":"img/jpg/w2560/secret_page.jpg",
         "alt":"Личный проект",
-        "data-path":"https://github.com/gusssar/bulletin-board",
+        "data_path":"https://github.com/gusssar",
         "title":"Постоянный личный проект, который я делаю в свое свободное время."
     },
     "pic_5": {
         "href":"img/jpg/w2560/next_page.jpg",
         "alt":"Дальше жарче",
-        "data-path":"mailto:a.alexeev.mai@yandex.ru",
+        "data_path":"mailto:a.alexeev.mai@yandex.ru",
         "title":"Может я Вам смогу помочь с проектом, обязательно свяжитесь со мной"
     }
 }
 /**
  * TODO: Добавить цикличность при простое
- * добавить что бы первый элемент фокусился сразу
  * стили-стили
  * подумать над мобильной версией
  */
 const img_wrap=document.getElementById('_img-preview');
 const item=document.getElementsByClassName('item');
-
-img_wrap.style.background="#f3f3f3 no-repeat center top";//вынести в css
+/**
+ * выставляем дефолтный фокус на первый элемент
+ */
+item[0].classList.add("item__focused");
 
 /**Циклом навешиваем id для картинок
  * а также слушатели для фокус/блёр
  */
 for (let i=0; i<item.length; i++){
     item[i].id="pic_"+i;
+    item[i].innerHTML=data[item[i].id].alt;
     item[i].addEventListener('mouseover',onFocus);
     item[i].addEventListener('mouseout',onBlur);
 }
 /** 
+ * очищаем все фокусы
  * фокусом навешиваем класс
  * и выбираем картинку
  */
 function onFocus(event){
+    for (let i=0; i<item.length; i++){
+        item[i].classList.remove("item__focused");
+    }
     event.target.classList.add("item__focused");
     SetImage(event.target);
+    onOpenPopUp(event.target);
 }
 /** 
- * Блёром просто снимаем фокус
+ * Блёром снимаем фокус
+ * если фокусим на item
  * картинку не трогаем
  */
 function onBlur(event){
-    // el.target.style.color="black"
-    event.target.classList.remove("item__focused");
+    if(event.relatedTarget.classList[0]==='item'){
+        event.target.classList.remove("item__focused");
+        //убираем сабскрайб при разблёре
+    }
+    document.getElementsByClassName('item__subscribe')[0].remove();
 }
 /**
  * влупливаем превьюху
@@ -81,4 +92,32 @@ function SetImage(el){
     img_wrap.style.backgroundImage="url("+_bg+")"
 }
 
-// SetImage();
+/**
+ * заполняем попап
+ */
+function onOpenPopUp(el){
+    let _node = data[el.id];
+    //обёртка для попапа
+    let _ul = document.createElement("ul");
+    _ul.classList.add('item__subscribe');
+    //заполняем объяснения
+    let _li_sub = document.createElement("li");
+        _li_sub.innerHTML=_node.title;
+    //заполняем ссылки
+    let _li_link = document.createElement("li");
+    _ul.appendChild(_li_sub);
+    _ul.appendChild(_li_link);
+
+    el.appendChild(_ul);
+
+}
+
+/**
+ * вешаем ссылки на превьюхи
+ * а также слушателя клика
+ */
+img_wrap.addEventListener('click',onClickPreview);
+function onClickPreview(){
+    let _id = document.getElementsByClassName("item__focused")[0].id;
+    window.location=data[_id].data_path;
+}
